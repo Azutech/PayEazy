1259057f0e76c6c76a84324ca532c17907dc1ac7
Azutech <azunna.onugha@gmail.com>
2026-03-16 12:33:34 +0100

    add_feat () : Server started for users

--- apps/users/src/app.module.ts
+++ apps/users/src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './users/auth.module';
 import { DatabaseModule } from '../../../libs/database/src/prisma.module';
 
 @Module({
-  imports: [AuthModule, DatabaseModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
