1082e27a9724f55b0f60163d7ced0f53ee877962
Azutech <azunna.onugha@gmail.com>
2026-03-17 01:41:23 +0100

    feat: Configure the application as a gRPC microservice with dynamic port retrieval from `ConfigService` and enhanced startup logging.

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 import { UsersModule } from './app.module';
+import { join } from 'path';
 
 async function bootstrap() {
+  const logger = new Logger('Users-Service');
   const app = await NestFactory.create(UsersModule);
-  await app.listen(process.env.port ?? 3000);
+  const configService = app.get(ConfigService);
+
+  const port = configService.get<string>('USER_PORT') as string;
+
+  console.log(port);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      package: 'user',
+      protoPath: join(__dirname, 'user.proto'),
+    },
+  });
+
+  await app.listen(port, () => logger.log(`App running on Port: ${port}`));
 }
 bootstrap();
